1fea9d5007da91c51fe4acdb32afc73e8073b01b
aca123321 <isamudri1@gmail.com>
2025-01-16 23:48:52 +0530

    support for number of tables in .dbinfo

--- app/main.js
+++ app/main.js
@@ -7,9 +7,9 @@ if (command === ".dbinfo") {
   const databaseFileHandler = await open(databaseFilePath, "r");
 
   const { buffer } = await databaseFileHandler.read({
-    length: 100,
+    length: 108,
     position: 0,
-    buffer: Buffer.alloc(100),
+    buffer: Buffer.alloc(108),
   });
 
   // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -18,6 +18,12 @@ if (command === ".dbinfo") {
   // Uncomment this to pass the first stage
   const pageSize = buffer.readUInt16BE(16); // page size is 2 bytes starting at offset 16
   console.log(`database page size: ${pageSize}`);
+
+  // const fileFormatWriteVersion = buffer.readUInt8(18);
+  // console.log(`database file format write version: ${fileFormatWriteVersion}`);
+
+  const cell_count = buffer.readUInt16BE(103); // cell count is 3 bytes starting from the end of file header
+  console.log(`number of tables: ${cell_count}`);
 } else {
   throw `Unknown command ${command}`;
 }
